aa3a2023d7dfc7b2145be025d38f94d6c8ecc285
kuijntjes <kuijntjes@physics.leidenuniv.nl>
2018-11-07 16:40:56 +0100

    normal distributed radius

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 print ("Hello world! Blup")
 
-import numpy as np
-import matplotlib as plt
+import numpy as np 
+import matplotlib.pyplot as plt
 
-r = np.random.random_sample((60))
+n_samples = int(1E4)
+sigma = 10
+mu = 3
+
+r = np.random.normal(loc=mu, scale=sigma, size=n_samples)
+
 circumference = 2 * np.pi * r
-
-
-plt.hist(circumference, bins=20)
-
+plt.hist(circumference, bins=100)
 
